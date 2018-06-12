a3783d5a10f955f95a63f15022e118a967e08005
HenriqueSD <hsd.foz@gmail.com>
2018-06-11 22:06:03 -0300

    Configured Exception Handling.

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -22,7 +22,10 @@ namespace WishList
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseMvcWithDefaultRoute();
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+            else
+                app.UseExceptionHandler("/Home/Error");
         }
     }
 }
